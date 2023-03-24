0ff05ae83e3345dbd99be5ea9f1cf4a33384c13c
aTfure <atfure@gmail.com>
2023-03-24 00:42:08 +0000

    README file

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Create a function def pascal_triangle(n): that returns a list of lists
+of integers representing the Pascal's triangle of n:
+"""
+triangle = []
+
+
+def pascal_triangle(n):
+    """
+    Return a list of lists, or a null list if n <=0
+    """
+    if n <= 0:
+        return []
+    for x in range(1, n+1):
+        if x == 1:
+            triangle.append([1])
+            continue
+        triangle.append(get_floor(triangle[x - 2]))
+    return triangle
+
+
+def get_floor(floor):
+    """
+    Creates the triangle's floor
+    """
+    floor_list = [1, 1]
+    size = len(floor) - 1
+
+    if size > 0:
+        for i in range(size):
+            floor_list.insert(i + 1, floor[i] + floor[i + 1])
+    return floor_list
