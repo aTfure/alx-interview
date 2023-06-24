d71a9bed0428000f19ec55f1f3b53ad5274e5e5c
Fred Onyango <atfure@gmail.com>
2023-06-25 02:16:40 +0300

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,23 +1,49 @@
 #!/usr/bin/python3
-"""0. Prime Game
-"""
+
+""" Prime Game """
+
+
+def check_prime(n):
+    """ Check if n is a prime number """
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
+            return False
+    return True
+
+
+def add_prime(n, primes):
+    """ Add prime to list """
+    last_prime = primes[-1]
+    if n > last_prime:
+        for i in range(last_prime + 1, n + 1):
+            if check_prime(i):
+                primes.append(i)
+            else:
+                primes.append(0)
 
 
 def isWinner(x, nums):
-    """determines the winner of the game"""
-    participants = {'Maria': 0, 'Ben': 0}
-    if x == 0 or x is None:
-        return
-    for i in range(x):
-        vals = [num for num in range(1, nums[i] + 1)]
-        currentTurn = 'Maria'
-        while vals != []:
-            currentPick = vals.pop(0)
-            for num in vals:
-                if num % currentPick == 0:
-                    vals.remove(num)
-            currentTurn = 'Ben' if currentTurn == 'Maria' else 'Maria'
-        participants[currentTurn] += 1
-    if participants['Maria'] > participants['Ben']:
-        return 'Maria'
-    return 'Ben'
+    """ x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None """
+
+    score = {"Maria": 0, "Ben": 0}
+    primes = [0, 0, 2]
+    add_prime(max(nums), primes)
+
+    for round in range(x):
+        _sum = sum((i != 0 and i <= nums[round])
+                   for i in primes[:nums[round] + 1])
+        if (_sum % 2):
+            winner = "Maria"
+        else:
+            winner = "Ben"
+        if winner:
+            score[winner] += 1
+
+    if score["Maria"] > score["Ben"]:
+        return "Maria"
+    elif score["Ben"] > score["Maria"]:
+        return "Ben"
+
+    return None
